8732bb983d3f842fc80a9ee5fe2a08038b554319
Jason Frame <jason@onehackoranother.com>
2013-11-13 23:38:16 +0000

    initial code

---
+++ README.md
@@ -0,0 +1,27 @@
+# style-tag
+
+Create `&lt;style&gt;` elements dynamically in the browser.
+
+## Install
+
+    $ npm install style-tag
+
+Use Browserify.
+
+## Usage
+
+    var styletag = require('style-tag');
+
+    // create a <style> tag and return a function for updating the CSS
+    // 2nd arg is the initial CSS
+    var setter = styletag(document, 'h1 { color: red; }');
+
+    // update the CSS
+    setter('h1 { color: green; }');
+
+    // access the actual <style> element itself
+    console.log(setter.el);
+
+## License
+
+MIT.
---
+++ index.js
@@ -0,0 +1,34 @@
+// adapted from
+// http://stackoverflow.com/questions/524696/how-to-create-a-style-tag-with-javascript
+module.exports = function(doc, initialCss) {
+    
+    if (typeof doc === 'string') {
+        initialCss = doc;
+        doc = null;
+    }
+
+    doc = doc || document;
+
+    var head    = doc.getElementsByTagName('head')[0],
+        style   = doc.createElement('style');
+
+    style.type = 'text/css';
+    head.appendChild(style);
+
+    function set(css) {
+        css = '' + (css || '');
+        if (style.styleSheet) {
+            style.styleSheet.cssText = css;
+        } else {
+            style.removeChild(style.firstChild);
+            style.appendChild(doc.createTextNode(css));
+        }
+    }
+
+    set(initialCss || '');
+
+    set.el = style;
+
+    return set;
+
+}
